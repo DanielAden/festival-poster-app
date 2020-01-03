4513d768642b05e91231565a07d9b1a625b64760
Daniel Aden <danieladen@Daniels-MacBook-Pro.local>
2020-01-03 13:19:21 -0800

    Remove old spotify api test

--- src/spotify/SpotifyAPI.test.ts
+++
@@ -1,6 +0,0 @@
-import {SPOTIFY_API_HOST, apiurl} from './SpotifyAPI'
-
-test('spotify api url', () => {
-  let res = apiurl('test1', 'test2', 'test3');
-  expect(res).toBe(`${SPOTIFY_API_HOST}/v1/test1/test2/test3`)
-})
