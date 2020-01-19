84f26ec6090e40095a30670cc33f3b7275d6d3a1
Daniel Aden <danieladen@Daniels-MacBook-Pro.local>
2020-01-18 16:31:45 -0800

    Updates

---
+++ precache-manifest.b7abf6599c4d6a5e617ebd819d6cf3da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8a1146543a01f68b70428da1690bdb",
+    "url": "/festival-poster-app/index.html"
+  },
+  {
+    "revision": "2d9fc66952d8b0d0d3b6",
+    "url": "/festival-poster-app/static/css/main.870b1b3f.chunk.css"
+  },
+  {
+    "revision": "d93ae71a77534df96b04",
+    "url": "/festival-poster-app/static/js/2.1d7590b7.chunk.js"
+  },
+  {
+    "revision": "68aed49bcdcbc067487a3d461801ff7e",
+    "url": "/festival-poster-app/static/js/2.1d7590b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d9fc66952d8b0d0d3b6",
+    "url": "/festival-poster-app/static/js/main.dab55108.chunk.js"
+  },
+  {
+    "revision": "e5bd0bda25262c62530b",
+    "url": "/festival-poster-app/static/js/runtime-main.81049c0c.js"
+  },
+  {
+    "revision": "7878fa911036f6ac08fd542a5331cb9c",
+    "url": "/festival-poster-app/static/media/city.7878fa91.jpg"
+  },
+  {
+    "revision": "c1f1ed0cbe4a6a1578cd7f13dffe38b5",
+    "url": "/festival-poster-app/static/media/fireworks.c1f1ed0c.jpg"
+  }
+]);
