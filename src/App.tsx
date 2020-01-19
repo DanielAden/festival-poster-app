098f2c2417f96ceaeb7fc948019fbc5b4abc8365
Daniel Aden <danieladen@Daniels-MacBook-Pro.local>
2020-01-18 16:33:37 -0800

    adjusted auth logic to not redirect

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,26 @@ import {
   Route,
 } from "react-router-dom";
 import SpotifyAuthorize from './components/SpotifyAuthorize';
+import { useDispatch } from 'react-redux';
+import { spotifyAuthFromWindow } from './spotify/SpotifyAuth';
+import { accessTokenUpdated } from './store/system/systemSlice';
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const authData = spotifyAuthFromWindow();
+
+  // TODO update this to be safer
+  if (authData.status !== 'AUTHORIZED') {
+    // console.log(`Non Authorized status: ${authData.status}`)
+    // console.log(authData.error)
+  } else {
+    if (!authData.data) throw new Error('Received authorized status without data')
+    const { access_token, expires_in }  = authData.data;
+    window.location.hash = '';
+    dispatch(accessTokenUpdated({ spotifyAccessToken: access_token, spotifyAccessTokenExpire: expires_in}))
+  }
+
+
   return (
     <div className="App">
       <Switch>
