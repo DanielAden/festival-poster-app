f29b7c5605898c9641d2c2628d58d44d10d83dd3
Daniel Aden <danieladen@Daniels-MacBook-Pro.local>
2020-02-03 16:38:08 -0800

    refactored font package from class to object

---
+++ src/components/Poster/FontPackage.ts
@@ -0,0 +1,13 @@
+export interface PosterTextStrokeInfo {
+  strokeStyle: string;
+  widthRatio: number;
+  offsetX: number;
+  offsetY: number;
+}
+
+export interface FontPackage {
+  fontType: string;
+  fontColor: string;
+  fontSizeRatio: number;
+  strokeInfo?: PosterTextStrokeInfo;
+}
